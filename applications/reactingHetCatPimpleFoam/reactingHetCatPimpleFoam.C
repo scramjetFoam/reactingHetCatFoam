551af7f69fd52593f436c64b56f0a8c6452e82ff
Tomas Hlavaty <t.hlavaty@outlook.com>
2022-08-02 11:18:12 +0200

    First version of the Pimple solver
    Note: not prepared for heat transfer in porous media.
    Prepared for homogeneus reactions

---
+++ applications/reactingHetCatPimpleFoam/reactingHetCatPimpleFoam.C
@@ -0,0 +1,206 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    diffReactConvSysThreeWayMacro
+
+Description
+    Diffusion-reaction-convection solver for macro-scale model of a 
+    monolithic catalytic filter. Three way catalytic coating is assumed.
+    
+    Solver can process in-wall and on-wall coating separately with
+    different reaction constants (based on the volumetric fraction of 
+    catalytic coating in on-wall and in-wall layer)
+    
+    In-wall/on-wall regions are stored in different cellZones
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "fluidThermo.H"
+#include "compressibleMomentumTransportModels.H"
+#include "fluidThermophysicalTransportModel.H"
+#include "pimpleControl.H"
+// #include "pressureControl.H"
+#include "pressureReference.H"
+#include "CorrectPhi.H"
+// #include "fvOptions.H"
+#include "fvModels.H"
+#include "fvConstraints.H"
+#include "IOporosityModelList.H"
+// #include "Switch.H"
+#include "localEulerDdtScheme.H"
+#include "fvcSmooth.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+   
+    // #include "setRootCase.H"
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createDyMControls.H"
+    #include "readTransportPropertiesMy.H"
+    #include "postProcess.H"
+    #include "createFields.H"
+    #include "createFieldRefs.H"
+    //~ #include "createPorousZones.H"
+    #include "createZones.H"
+    // #include "createFvOptions.H"
+    #include "initContinuityErrs.H"
+    #include "createRhoUfIfPresent.H"
+
+    turbulence->validate();
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    if (!LTS)
+    {
+        #include "compressibleCourantNo.H"
+        #include "setInitialDeltaT.H"
+    }
+    
+    // while (simple.loop(runTime))
+    while (pimple.run(runTime))
+    {
+        #include "readDyMControls.H"
+
+        // Store divrhoU from the previous mesh so that it can be mapped
+        // and used in correctPhi to ensure the corrected phi has the
+        // same divergence
+        autoPtr<volScalarField> divrhoU;
+        if (correctPhi)
+        {
+            divrhoU = new volScalarField
+            (
+                "divrhoU",
+                fvc::div(fvc::absolute(phi, rho, U))
+            );
+        }
+
+        if (LTS)
+        {
+            #include "setRDeltaT.H"
+        }
+        else
+        {
+            #include "compressibleCourantNo.H"
+            #include "setDeltaT.H"
+        }
+
+        fvModels.preUpdateMesh();
+
+        // Store momentum to set rhoUf for introduced faces.
+        autoPtr<volVectorField> rhoU;
+        if (rhoUf.valid())
+        {
+            rhoU = new volVectorField("rhoU", rho*U);
+        }
+
+        // Update the mesh for topology change, mesh to mesh mapping
+        mesh.update();
+
+        runTime++;
+
+        Info<< "Time = " << runTime.userTimeName() << nl << endl;
+        
+        while (pimple.loop())
+        {
+            if (pimple.firstPimpleIter() || moveMeshOuterCorrectors)
+            {
+                // Move the mesh
+                mesh.move();
+
+                if (mesh.changing())
+                {
+                    MRF.update();
+
+                    if (correctPhi)
+                    {
+                        #include "correctPhi.H"
+                    }
+
+                    if (checkMeshCourantNo)
+                    {
+                        #include "meshCourantNo.H"
+                    }
+                }
+            }
+            
+            if
+            (
+                !mesh.schemes().steady()
+             && !pimple.simpleRho()
+             && pimple.firstPimpleIter()
+            )
+            {
+                #include "rhoEqn.H"
+            }
+
+            fvModels.correct();
+
+            Info << "\nSolving momentum equation"<<endl;
+            
+            #include "UEqn.H"
+
+            Info << "\nSolving continuity equation for each specie." << endl;
+            #include "concEq.H"
+            
+            Info << "\nSolving enthalpy balance." << endl;
+            #include "EEqn.H"
+            
+            // --- Pressure corrector loop
+            while (pimple.correct())
+            {
+                Info << "\nCorrection of pressure"<<endl;
+                #include "pEqn.H"   
+            }
+
+            if (pimple.turbCorr())
+            {
+                turbulence->correct();
+                thermophysicalTransport->correct();
+            }
+        }              
+                
+        if (!mesh.schemes().steady())
+        {
+            rho = thermo.rho();
+        }
+
+        runTime.write();
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
