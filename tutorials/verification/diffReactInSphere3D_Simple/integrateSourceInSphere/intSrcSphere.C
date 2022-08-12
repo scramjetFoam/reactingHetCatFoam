c95c971cbdccc3227ab750c7219bc150add474c2
Tomas Hlavaty <t.hlavaty@outlook.com>
2022-08-12 13:00:36 +0200

    Verification of the sphere case.

---
+++ tutorials/verification/diffReactInSphere3D_Simple/integrateSourceInSphere/intSrcSphere.C
@@ -0,0 +1,118 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    caclPressDerOnSlices
+
+Description
+    Calculates and prints average pressures over slices along some given
+    coordinate. In user specified box
+     
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "sampledPlane.H"
+#include <cstdlib>
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    instantList timeDirs = timeSelector::select0(runTime, args);
+    #include "createNamedMesh.H"
+
+    // Info <<timeDirs.size()<<endl;
+    runTime.setTime(timeDirs[timeDirs.size()-1], timeDirs.size()-1);
+    Info<< "Time = " << runTime.timeName() << endl;
+    mesh.readUpdate();
+    
+    // -- read molar fraction field
+    volScalarField CO 
+    (
+        IOobject
+            (
+            "CO", 
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh
+    );
+
+    // -- read rho field
+    volScalarField rho
+    (
+        IOobject
+            (
+            "rho", 
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh
+    );
+
+    // -- read molar mass
+    // -- thermophysicalProperties dictionary
+    IOdictionary thermophysicalProperties
+    (
+        IOobject
+        (
+        "thermophysicalProperties",    // dictionary name
+        runTime.constant(),     // dict is found in "constant"
+        mesh,                   // registry for the dict
+        IOobject::MUST_READ,    // must exist, otherwise failure
+        IOobject::NO_WRITE      // dict is only read by the solver
+        )
+    );
+    // -- molar mass
+    scalar molMR(readScalar(thermophysicalProperties.subDict("mixture").subDict("specie").lookup("molWeight")));
+    
+    // -- integration (sum c_s*V in cellZone)
+    scalar integral(0);
+
+    // -- loop over all cells in cellzone
+    word currentZone = "reactingCellZone";
+
+    labelList cellZoneIDs_ = mesh.cellZones().findIndices(currentZone);
+
+    const labelList& cells = mesh.cellZones()[cellZoneIDs_[0]];
+    
+    forAll(cells, celli)
+    {
+        integral += mesh.V()[celli]*CO[celli]*rho[celli]/molMR;
+    }
+    Info << "Integral reaction source = " << integral << endl;
+
+    Info<< "End" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
---
+++ tutorials/verification/diffReactInSphere3D_Simple/verDCRSphere.py
@@ -0,0 +1,111 @@
+# -- Script to do verification simulation of isothermal reaction inside sphere
+
+# -- imports
+import numpy as np
+import os 
+import shutil as sh
+
+# -- setup case
+baseCaseDir = 'baseCase'
+outFolder = 'ZZ_cases'
+
+# -- case data
+yInf = 0.01     # molar fraction farfar from sphere
+p = 101325      # presure
+Runiv = 8.314   # universal gas constant
+R = 1           # sphere radius
+Rinf = 3         # infinite radius 
+TLst = [500,550] # temperature 
+k0 = 5e7      # reaction pre-exponential factor
+EA = 90e3     # reaction activation energy
+tort = 3      # tortuosity
+solver = 'reactingHetCatSimpleFoam' # used solver
+
+# -- create case
+for T in TLst:
+    # -- create caseFolder based on baseCase
+    caseName = 'yInf_%g_R_%g_Rinf_%g_T_%g_tort_%g'%(yInf,R,Rinf,T,tort)
+    print('Preparing case %s'%caseName)
+    caseDir = '%s/%s/'%(outFolder,caseName)
+
+    if os.path.isdir(caseDir):                                          #ensure, that the caseDir is clear
+        sh.rmtree(caseDir)
+
+    sh.copytree(baseCaseDir,caseDir)
+
+
+    # -- change the case files
+    # NOTE TH: I only change temperature, much more can be studied 
+    print('Changing temperature to %g'%T)
+    with open(caseDir + '/0.org/T', 'r') as file:
+        data = file.readlines()
+
+    for ind in range(len(data)):
+        if data[ind].find('isoT') >= 0:#!!do not put comment on executed temperature
+            data[ind] = data[ind].replace('isoT',str(T))
+
+    with open(caseDir + '/0.org/T', 'w') as file:
+        file.writelines(data)
+    
+    # -- run the simulation
+    os.chdir(caseDir)
+    os.system('./Allrun')
+
+    # -- save results
+    # -- load Dfree, Deff, and k from log file
+    print('Reading parameters from log file.')
+    # NOTETH: this can be slow (in that case maybe use something like tail?)
+    with open('log.%s'%solver, 'r') as fl:
+        lines = fl.readlines()
+    # -- go from back to get latest result
+    #DFree
+    for lineInd in range(len(lines)-1,0,-1):
+        if lines[lineInd].find('DFree') >= 0:
+            DFree = float(lines[lineInd].split(' ')[-1].replace('\n',''))
+            print('DFree = %g'%DFree)
+            break
+        if lineInd == 0:
+            print('DFree not found.')
+    #DEff
+    for lineInd in range(len(lines)-1,0,-1):
+        if lines[lineInd].find('DEff') >= 0:
+            DEff = float(lines[lineInd].split(',')[0].split(' ')[-1].replace('\n',''))
+            print('DEff = %g'%DEff)
+            break
+        if lineInd == 0:
+            print('DEff not found.')
+    #k
+    for lineInd in range(len(lines)-1,0,-1):
+        if lines[lineInd].find('max(k)') >= 0:
+            k = float(lines[lineInd].split(' ')[-1].replace(').\n',''))
+            print('k = %g'%k)
+            break
+        if lineInd == 0:
+            print('k not found.')
+
+    # -- compute analytical results
+    # NOTETH: this is probably wrong
+    kL = 2*DFree/(R-Rinf)         # mass transfer coefficient (absolutely not sure about this)
+
+    thiele = R*(k/DEff)**(0.5)  # thiele modulus
+    BiM = kL*R/DEff             # Biot number
+    etaAnal = 3./(thiele**2) * (thiele*1./np.tanh(thiele)-1)/(1+(thiele*1./np.tanh(thiele)-1)/BiM)                      # analytical effectivness factor
+    print('\nBiot number = %g\nThiele modulus = %g\nAnalytical effectivness factor is %g'%(BiM,thiele,etaAnal))
+
+    # -- compute simulation results
+    # -- read real source
+    with open('log.intSrcSphere', 'r') as fl:
+        lines = fl.readlines()
+    #reaction source
+    for lineInd in range(len(lines)-1,0,-1):
+        if lines[lineInd].find('reaction source') >= 0:
+            rS = float(lines[lineInd].split(' ')[-1].replace('\n',''))
+            break
+        if lineInd == 0:
+            print('Reaction source not found.')
+    rSqIdeal = 4./3*np.pi*R**3*k*yInf*p/Runiv/T
+    etaSim = rS/rSqIdeal
+    print('Simulation effectivness factor is %g, relative error = %g'%(etaSim,(etaAnal-etaSim)/etaAnal))
+    
+
+    os.chdir('../../')
