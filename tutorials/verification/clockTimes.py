80186628b874c1ea6eb595242f8b34340429d5b7
khyrm <khyr.matyas@gmail.com>
2023-03-23 17:45:57 +0100

    Updates to auxiliary scripts.

--- tutorials/verification/clockTimes.py
+++ tutorials/verification/clockTimes.py
@@ -1,11 +1,12 @@
 from os import walk
 from os.path import isfile
+import os
 
 
-verCases = [dir for dir in next(walk('.'))[1] if 'baseCase' not in dir and 'inner' in dir]
-# verCases = [dir for dir in next(walk('.'))[1] if 'baseCase' not in dir and 'flow' in dir]
+# verCases = [dir for dir in next(walk('.'))[1] if 'baseCase' not in dir and 'inner' in dir]
+verCases = [dir for dir in next(walk('.'))[1] if 'baseCase' not in dir and 'flow' in dir]
 # verCases = [dir for dir in next(walk('.'))[1] if 'baseCase' not in dir]
-# print(verCases)
+print(verCases)
 for verCase in verCases:
     ZZ_dir = verCase+'/ZZ_cases'
     solver = 'reactingHetCatSimpleFoam'
@@ -17,11 +18,18 @@ for verCase in verCases:
     timessum = 0
     timesnum = 0
     for caseDir in caseDirs:
+        print('incpecting %s->%s'%(verCase[-4:],caseDir))
         if caseDir != 'protoMesh':
+            
             caseDir = ZZ_dir+'/'+caseDir+'/'
             log1 = caseDir+'log1.'+solver
             log2 = caseDir+'log2.'+solver
             time = 'not found'
+
+            # wronglog1 = caseDir+'log.'+solver+'1'
+            # wronglog2 = caseDir+'log.'+solver+'2'
+            # if isfile(wronglog1): os.system('mv %s %s'%(wronglog1,log1))
+            # if isfile(wronglog2): os.system('mv %s %s'%(wronglog2,log2))
 
             if isfile(log2): log = log2
             else: log = log1
